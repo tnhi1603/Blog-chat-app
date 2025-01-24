5abcbc8c6c8d43db81701e4093221e3cb24794c4
tnhi1603 <21521236@gm.uit.edu.vn>
2025-01-24 23:16:05 +0700

    login

---
+++ src/layout/Login/LoginPage.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { FaFacebook, FaWhatsapp, FaTelegram } from 'react-icons/fa';
+import './LoginPage.css';
+import Image from '../../assets/login.png';
+import welcome from '../../assets/welcome.gif';
+
+const LoginPage = () => {
+  return (
+    <div className="register-container">
+      <div class="background-accent"></div>
+      <div className="form-container">
+        <div className="welcome-gif">
+          <img src={welcome} alt="Welcome Animation" />
+        </div>
+        <h2>Please Fill out form to Login</h2>
+        <form>
+          <input type="text" placeholder="Username or Email" />
+          <input type="password" placeholder="Password" />
+          <button type="submit">Login</button>
+        </form>
+        <p>
+          Don't have an account? <a href="/register">Register</a>
+        </p>
+        <div className="social-icons">
+          <a href="/facebook" className="icon">
+            <FaFacebook />
+          </a>
+          <a href="/whatsapp" className="icon">
+            <FaWhatsapp />
+          </a>
+          <a href="/telegram" className="icon">
+            <FaTelegram />
+          </a>
+        </div>
+      </div>
+      <div className="illustration">
+        <img src={Image} alt="Illustration" />
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -7,11 +7,13 @@ import Friends from './Friends';
 import Profile from './Profile';
 import Blog from './Blog';
 import Register from './Register';
+import Login from './Login';
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
       <Route path="/home" element={<Home />} />
       <Route path="/notifications" element={<Notifications />} />
       <Route path="/chat" element={<Chat />} />
