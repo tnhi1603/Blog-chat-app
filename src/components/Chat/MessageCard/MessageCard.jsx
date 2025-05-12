8d7d27c88bdd80c9965125c67c32ca65526da5f3
tnhi1603 <21521236@gm.uit.edu.vn>
2025-05-12 20:50:33 +0700

    update

--- src/components/Chat/MessageCard/MessageCard.jsx
+++ src/components/Chat/MessageCard/MessageCard.jsx
@@ -5,12 +5,12 @@ import "./MessageCard.css";
 const MessageCard = ({ isSender, avatar, message, time }) => {
   return (
     <div className={`message-card ${isSender ? "sent" : "received"}`}>
-      {!isSender && <img src={avatarImage} alt="User Avatar" className="message-avatar" />}
+      {!isSender && <img src={avatar} alt="User Avatar" className="message-avatar" />}
       <div className="message-content">
         <p className="message-text">{message}</p>
         <span className="message-time">{time}</span>
       </div>
-      {isSender && <img src={avatarImage} alt="User Avatar" className="message-avatar" />}
+      {isSender && <img src={avatar} alt="User Avatar" className="message-avatar" />}
     </div>
   );
 };
